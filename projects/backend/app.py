46f438e2e6bf7c2f1067f247727eb670cd8fec85
Kaustubh2512 <ksandure@gmail.com>
2026-05-21 22:42:20 +0530

    deployemt fixed

--- projects/backend/app.py
+++ projects/backend/app.py
@@ -72,6 +72,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "https://*.vercel.app",
+        "https://algo-shield-ai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
